58fb5bdccb3914d6ef60164b923ae0bd98b6dfb3
Neokkers <mendelson.inbox@gmail.com>
2019-10-21 15:29:29 +0300

    takumi

--- takumi/src/js/index.js
+++ takumi/src/js/index.js
@@ -1,1 +1,2 @@
+import "../styles/utils/fonts.css";
 import "../styles/main.scss";
--- takumi/webpack.config.js
+++ takumi/webpack.config.js
@@ -25,7 +25,29 @@ module.exports = {
           "postcss-loader",
           "sass-loader"
         ]
+      }, {
+          test: /\.(eot|svg|ttf|woff|woff2)$/,
+          use: [
+                   {
+                       loader: 'file-loader?name=./assets/fonts/webfonts/[name].[ext]'
+                   },
+                   {
+                       loader: 'file-loader?name=./assets/fonts/Roboto/[name].[ext]'
+                   }
+               ]
       }
+      // {test: /\.(jpg|jpeg|png|woff|woff2|eot|ttf|svg)$/,loader: 'url-loader?limit=10000'}
+      // {
+      //   test: /\.(ttf|eot|woff|woff2)$/,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: {
+      //         name: 'fonts/[name].[ext]',
+      //       }
+      //     }
+      //   ]
+      // }
     ]
   },
   plugins: [
